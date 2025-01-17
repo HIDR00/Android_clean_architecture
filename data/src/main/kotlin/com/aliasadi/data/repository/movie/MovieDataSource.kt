5a7c888ad95ad2f740e3aa7d44abfee2bc788531
Trần Toản <Tran Toan>
2025-01-17 15:12:40 +0700

    first commit

---
+++ data/src/main/kotlin/com/aliasadi/data/repository/movie/MovieDataSource.kt
@@ -0,0 +1,32 @@
+package com.aliasadi.data.repository.movie
+
+import androidx.paging.PagingSource
+import com.aliasadi.data.entities.MovieData
+import com.aliasadi.data.entities.MovieDbData
+import com.aliasadi.data.entities.MovieRemoteKeyDbData
+import com.aliasadi.domain.entities.MovieEntity
+import com.aliasadi.domain.util.Result
+
+/**
+ * Created by Ali Asadi on 13/05/2020
+ */
+interface MovieDataSource {
+
+    interface Remote {
+        suspend fun getMovies(page: Int, limit: Int): Result<List<MovieData>>
+        suspend fun getMovies(movieIds: List<Int>): Result<List<MovieData>>
+        suspend fun getMovie(movieId: Int): Result<MovieData>
+        suspend fun search(query: String, page: Int, limit: Int): Result<List<MovieData>>
+    }
+
+    interface Local {
+        fun movies(): PagingSource<Int, MovieDbData>
+        suspend fun getMovies(): Result<List<MovieEntity>>
+        suspend fun getMovie(movieId: Int): Result<MovieEntity>
+        suspend fun saveMovies(movies: List<MovieData>)
+        suspend fun getLastRemoteKey(): MovieRemoteKeyDbData?
+        suspend fun saveRemoteKey(key: MovieRemoteKeyDbData)
+        suspend fun clearMovies()
+        suspend fun clearRemoteKeys()
+    }
+}
